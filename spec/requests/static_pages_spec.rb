6f7f05f850ce59fd90b15c7eb4f115700e854ec7
khanhld <le.duy.khanh@framgia.com>
2012-10-03 15:20:51 +0700

    Add full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,6 +2,8 @@ require 'spec_helper'
 
 describe "Static pages" do
 
+	let(:base_title) { "ARCHER" }
+
   describe "Home page" do
 
     it "should have the content 'Unlimited Blade Work'" do
@@ -9,10 +11,10 @@ describe "Static pages" do
       page.should have_content('Unlimited Blade Work')
     end
 
-    it "should have the title 'Chant'" do
-      visit '/static_pages/home'
-      page.should have_selector('title',:text => " | Chant")
-    end
+		it "should have the base title" do
+			visit '/static_pages/home'
+			page.should have_selector('title',:text => "#{base_title}")
+		end
   end
 
   describe "Help page" do
@@ -24,7 +26,7 @@ describe "Static pages" do
 
     it "should have the title 'Manual'" do
       visit '/static_pages/help'
-      page.should have_selector('title',:text => " | Manual")
+      page.should have_selector('title',:text => "#{base_title} | Manual")
     end
   end
 
@@ -37,20 +39,20 @@ describe "Static pages" do
 
 		it "should have the title 'Information'" do
 			visit '/static_pages/about'
-			page.should have_selector('title',:text => " | Information")
+			page.should have_selector('title',:text => "#{base_title} | Information")
 		end
   end
 
 	describe "Contact page" do
 		
 		it "should have the h1 'Servant Skill'" do
-			visit '/static_pages/about'
+			visit '/static_pages/contact'
 			page.should have_selector('h1',:text => "Servant Skill")
 		end
 
 		it "should have the title 'Skill'" do
-			visit '/static_pages/about'
-			page.should have_selector('title',:text => " | Skill")
+			visit '/static_pages/contact'
+			page.should have_selector('title',:text => "#{base_title} | Skill")
 		end
 	end
 end
