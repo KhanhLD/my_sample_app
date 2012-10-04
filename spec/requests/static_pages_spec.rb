6a6a29e26010d53fa3805e08e23b8327c49a9955
khanhld <le.duy.khanh@framgia.com>
2012-10-04 11:08:25 +0700

    layout and routing

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -4,55 +4,37 @@ describe "Static pages" do
 
 	let(:base_title) { "ARCHER" }
 
+	subject { page }
+
   describe "Home page" do
+		before {visit root_path}
 
-    it "should have the content 'Unlimited Blade Work'" do
-      visit '/static_pages/home'
-      page.should have_content('Unlimited Blade Work')
-    end
+    it { should have_content('Unlimited Blade Work') }
 
-		it "should have the base title" do
-			visit '/static_pages/home'
-			page.should have_selector('title',:text => "#{base_title}")
-		end
+		it { should have_selector('title', text: full_title('')) }
   end
 
   describe "Help page" do
+		before {visit help_path}
 
-    it "should have the content 'Mana is sufficient'" do
-      visit '/static_pages/help'
-      page.should have_content('Mana is sufficient')
-    end
+    it { should have_content('Mana is sufficient') }
 
-    it "should have the title 'Manual'" do
-      visit '/static_pages/help'
-      page.should have_selector('title',:text => "#{base_title} | Manual")
-    end
+    it { should have_selector('title', text: full_title('Manual')) }
   end
 
   describe "About page" do
+		before {visit about_path}
 
-    it "should have the content 'Servant Information'" do
-      visit '/static_pages/about'
-      page.should have_content('Servant Information')
-    end
+    it { should have_content('Servant Information') }
 
-		it "should have the title 'Information'" do
-			visit '/static_pages/about'
-			page.should have_selector('title',:text => "#{base_title} | Information")
-		end
+		it { should have_selector('title', text: full_title('Information')) }
   end
 
 	describe "Contact page" do
-		
-		it "should have the h1 'Servant Skill'" do
-			visit '/static_pages/contact'
-			page.should have_selector('h1',:text => "Servant Skill")
-		end
+		before {visit contact_path}
 
-		it "should have the title 'Skill'" do
-			visit '/static_pages/contact'
-			page.should have_selector('title',:text => "#{base_title} | Skill")
-		end
+		it { should have_selector('h1', text: "Servant Skill") }
+
+		it { should have_selector('title', text: "#{base_title} | Skill") }
 	end
 end
