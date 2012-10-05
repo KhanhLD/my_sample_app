039c55cdbcf7e48844e359bf07421c09e70b4d42
khanhld <le.duy.khanh@framgia.com>
2012-10-05 14:29:19 +0700

    Make User model

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,39 +2,58 @@ require 'spec_helper'
 
 describe "Static pages" do
 
-	let(:base_title) { "ARCHER" }
-
 	subject { page }
+
+	shared_examples_for "all static pages" do
+		it {should have_selector('h1', text: heading)}
+		it {should have_selector('title', text: full_title(page_title))}
+	end
 
   describe "Home page" do
 		before {visit root_path}
+		let (:heading) {"Chant"}
+		let (:page_title) {""}
 
-    it { should have_content('Unlimited Blade Work') }
-
-		it { should have_selector('title', text: full_title('')) }
+    it_should_behave_like "all static pages"
+		it {should_not have_selector('title', text: " | Home")}
   end
 
   describe "Help page" do
 		before {visit help_path}
-
-    it { should have_content('Mana is sufficient') }
-
-    it { should have_selector('title', text: full_title('Manual')) }
+		let (:heading) {"Manual"}
+		let (:page_title) {"Manual"}
+		
+		it_should_behave_like "all static pages"
   end
 
   describe "About page" do
 		before {visit about_path}
+		let (:heading) {"Information"}
+		let (:page_title) {"Information"}
 
-    it { should have_content('Servant Information') }
-
-		it { should have_selector('title', text: full_title('Information')) }
+		it_should_behave_like "all static pages"
   end
 
 	describe "Contact page" do
 		before {visit contact_path}
+		let (:heading) {"Skill"}
+		let (:page_title) {"Skill"}
 
-		it { should have_selector('h1', text: "Servant Skill") }
+		it_should_behave_like "all static pages"
+	end
 
-		it { should have_selector('title', text: "#{base_title} | Skill") }
+	it "should have the right links on the layout" do
+		visit root_path
+		click_link "About"
+		page.should have_selector('title', text: full_title("Information"))
+		click_link "Help"
+		page.should have_selector('title', text: full_title("Manual"))
+		click_link "Contact"
+		page.should have_selector('title', text: full_title("Skill"))
+		click_link "Home"
+		click_link "Sign up now"
+		page.should have_selector('title', text: full_title("Sign up"))
+		click_link "holy grail war"
+		page.should have_selector('title', text: full_title(""))
 	end
 end
